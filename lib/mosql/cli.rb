b94d520e956862fdfc49072232b4c33cc1b2ad82
Jonathan Perichon <jperichon@gmail.com>
2016-06-01 20:10:16 -0700

    Test

--- lib/mosql/cli.rb
+++ lib/mosql/cli.rb
@@ -118,14 +118,7 @@ module MoSQL
 
     def connect_mongo
       @mongo = Mongo::Client.new(options[:mongo])
-      config = @mongo.command(:ismaster => 1)
-      if !config['setName'] && !options[:skip_tail]
-        log.warn("`#{options[:mongo]}' is not a replset.")
-        log.warn("Will run the initial import, then stop.")
-        log.warn("Pass `--skip-tail' to suppress this warning.")
-        options[:skip_tail] = true
-      end
-      options[:service] ||= config['setName']
+      @mongo.command(:ismaster => 1)
     end
 
     def connect_sql
